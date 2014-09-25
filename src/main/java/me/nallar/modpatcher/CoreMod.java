76f1a0cca3d0cddd04e5e8f138c5ea80ac5c9f69
Ross Allan <rallanpcl@gmail.com>
2014-09-25 14:40:32 +0100

    Log to file

--- src/main/java/me/nallar/modpatcher/CoreMod.java
+++ src/main/java/me/nallar/modpatcher/CoreMod.java
@@ -1,12 +1,21 @@
 package me.nallar.modpatcher;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.appender.FileAppender;
 
 import java.util.*;
 
 @IFMLLoadingPlugin.Name("ModPatcher")
 @IFMLLoadingPlugin.SortingIndex(1001) // Magic value, after deobf transformer.
 public class CoreMod implements IFMLLoadingPlugin {
+	private static void logToFile() {
+		FileAppender fa = FileAppender.createAppender("./logs/ModPatcher.log", "false", "false", "PatcherAppender", "true", "true", "true", null, null, "false", null, null);
+		fa.start();
+		((org.apache.logging.log4j.core.Logger) LogManager.getLogger("ModPatcher")).addAppender(fa);
+		((org.apache.logging.log4j.core.Logger) LogManager.getLogger("JavaPatcher")).addAppender(fa);
+	}
+
 	@Override
 	public String[] getASMTransformerClass() {
 		return new String[0];
@@ -24,6 +33,7 @@ public class CoreMod implements IFMLLoadingPlugin {
 
 	@Override
 	public void injectData(Map<String, Object> data) {
+		logToFile();
 		ModPatcher.modPatcherAsCoreModStartup();
 	}
 
