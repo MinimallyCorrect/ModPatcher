921a7aad6463b6e0f8c220e14b89a660d4fe65d6
Ross Allan <ross@nallar.me>
2016-03-01 19:37:59 +0000

    Log version in load hook

--- src/main/java/me/nallar/modpatcher/ModPatcherLoadHook.java
+++ src/main/java/me/nallar/modpatcher/ModPatcherLoadHook.java
@@ -7,7 +7,7 @@ class ModPatcherLoadHook {
 	private static final String VERSION = "@VERSION@".replace("-SNAPSHOT", "");
 
 	static void loadHook(ModPatcher.Version requiredVersion, String modPatcherRelease, int apiVersion) {
-		PatcherLog.trace("Loaded ModPatcher", new Throwable());
+		PatcherLog.info("Loaded ModPatcher. Version: @MOD_VERSION@ API version: " + API_VERSION);
 
 		if (API_VERSION != apiVersion) {
 			PatcherLog.warn("API version mismatch. Expected " + API_VERSION + ", got " + apiVersion);
