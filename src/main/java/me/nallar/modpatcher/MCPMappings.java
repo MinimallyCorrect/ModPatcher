f2d2325a7525131dc76460c68cc7007ddc775c5e
Ross Allan <ross@nallar.me>
2016-03-04 09:39:04 +0000

    Fix wrong SRG mapping type

--- src/main/java/me/nallar/modpatcher/MCPMappings.java
+++ src/main/java/me/nallar/modpatcher/MCPMappings.java
@@ -31,7 +31,7 @@ class MCPMappings extends Mappings {
 		try {
 			loadCsv(Mappings.class.getResourceAsStream("/methods.csv"), methodSeargeMappings);
 			loadCsv(Mappings.class.getResourceAsStream("/fields.csv"), fieldSeargeMappings);
-			loadSrg(Mappings.class.getResourceAsStream("/packaged.srg"));
+			loadSrg(Mappings.class.getResourceAsStream("/srg-mcp.srg"));
 		} catch (Exception e) {
 			PatcherLog.error("Failed to load MCP mappings", e);
 		}
