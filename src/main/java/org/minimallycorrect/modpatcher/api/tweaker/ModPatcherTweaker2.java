d05c3ee62292415cf3cafb0a9cb34b16d8f21f80
Alex Thomson <LXGaming@users.noreply.github.com>
2020-02-19 18:10:47 +1300

    Add support for Mixin 0.8

--- src/main/java/org/minimallycorrect/modpatcher/api/tweaker/ModPatcherTweaker2.java
+++ src/main/java/org/minimallycorrect/modpatcher/api/tweaker/ModPatcherTweaker2.java
@@ -17,20 +17,46 @@ public class ModPatcherTweaker2 implements ITweaker {
 		((List<String>) Launch.blackboard.get("TweakClasses")).add(ModPatcherTweaker2.class.getName());
 	}
 
+	@SuppressWarnings("unchecked")
 	private static void inject() {
 		LaunchClassLoaderUtil.addTransformer(ModPatcherTransformer.getInstance());
+		
+		// Mixin 0.7
 		try {
 			Class<?> mixinEnvironmentClass = Class.forName("org.spongepowered.asm.mixin.MixinEnvironment", false, ModPatcherTweaker.class.getClassLoader());
-			Field f = mixinEnvironmentClass.getDeclaredField("excludeTransformers");
-			f.setAccessible(true);
-			@SuppressWarnings("unchecked")
-			Set<String> vals = (Set<String>) f.get(null);
-			vals.add(ModPatcherTransformer.class.getName());
-		} catch (ClassNotFoundException ignored) {
+			
+			// org.spongepowered.asm.mixin.MixinEnvironment.excludeTransformers
+			Field excludeTransformersField = mixinEnvironmentClass.getDeclaredField("excludeTransformers");
+			excludeTransformersField.setAccessible(true);
+			
+			Set<String> values = (Set<String>) excludeTransformersField.get(null);
+			values.add(ModPatcherTransformer.class.getName());
+		} catch (ClassNotFoundException ex) {
 			// TODO Silence this once confirmed working?
-			PatcherLog.trace("Failed to find mixin environment, normal for non-spongepowered", ignored);
-		} catch (NoSuchFieldException | IllegalAccessException e) {
-			PatcherLog.warn("Failed to add mixin transformer exclusion for our transformer", e);
+			PatcherLog.trace("Failed to find mixin environment, normal for non-spongepowered", ex);
+			return;
+		} catch (NoSuchFieldException ex) {
+			// no-op
+		} catch (Exception ex) {
+			PatcherLog.warn("Failed to add mixin transformer exclusion for our transformer", ex);
+			return;
+		}
+		
+		// Mixin 0.8
+		try {
+			Class<?> mixinServiceLaunchWrapperClass = Class.forName("org.spongepowered.asm.service.mojang.MixinServiceLaunchWrapper", false, ModPatcherTweaker.class.getClassLoader());
+			
+			// org.spongepowered.asm.service.mojang.MixinServiceLaunchWrapper.excludeTransformers
+			Field excludeTransformersField = mixinServiceLaunchWrapperClass.getDeclaredField("excludeTransformers");
+			excludeTransformersField.setAccessible(true);
+			
+			Set<String> values = (Set<String>) excludeTransformersField.get(null);
+			values.add(ModPatcherTransformer.class.getName());
+		} catch (ClassNotFoundException ex) {
+			// TODO Silence this once confirmed working?
+			PatcherLog.trace("Failed to find mixin environment, normal for non-spongepowered", ex);
+		} catch (Exception ex) {
+			PatcherLog.warn("Failed to add mixin transformer exclusion for our transformer", ex);
 		}
 	}
 
